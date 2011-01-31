4d3d39f14da937a6636967019cd62e591953a15c
Janet (PCF) <jed@pculture.org>
2011-01-31 15:58:44 -0500

    start of feedstests subgroup 16

---
+++ sg16_feedstests.sikuli/sg16_feedstests.py
@@ -0,0 +1,141 @@
+import sys
+import os
+import glob
+import unittest
+import StringIO
+import time
+
+mycwd = os.path.join(os.getcwd(),"Miro")
+sys.path.append(os.path.join(mycwd,'myLib'))
+import config
+import mirolib
+import testvars
+import litmusresult
+
+
+
+setBundlePath(config.get_img_path())
+
+
+class Miro_Suite(unittest.TestCase):
+    """Subgroup 16 - one-click subscribe tests.
+
+    """
+    def setUp(self):
+        self.verificationErrors = []
+        setAutoWaitTimeout(60)
+         
+
+
+    def test_74(self):
+        """http://litmus.pculture.org/show_test.cgi?id=74 Feed search, saved search feed
+
+        1. Add feed1, RSS 2.0 with Yahoo enclosures
+        2. Perform a search and save it.
+        3. Verify Search saved
+        4. Cleanup
+
+        """
+        miroRegions = mirolib.launch_miro()
+        s = miroRegions[0] #Sidebar Region
+        m = miroRegions[1] #Mainview Region
+        t = miroRegions[2] #top half screen
+        tl = miroRegions[3] #top left quarter
+        mtb = miroRegions[4] #main title bar
+        
+        url = "http://pculture.org/feeds_test/feed1.rss"
+        feed = "Yahoo Media TEST"
+        term = "first test video"
+        title = "Video 1"
+        
+        #1. add feed
+        mirolib.add_feed(self,t,s,mtb,url,feed)
+        #2. search
+        mirolib.tab_search(self,m,s,term)
+        
+        #3. verify item metadata
+        self.assertTrue(m.exists(title))
+        self.assertTrue(m.exists("This is the first test video"))
+        self.assertTrue(m.exists("http://participatoryculture.org/feeds_test/mike_tv_drawing_cropped.jpg"))
+        self.assertTrue(m.exists("842 B"))
+        #4. cleanup
+        mirolib.delete_feed(self,m,s,feed)
+
+    def test_75(self):
+        """http://litmus.pculture.org/show_test.cgi?id=75 Absolute and relative links.
+
+        1. Feed 1
+        2. View videos 3
+        3. Click the links and verify they are opened
+        4. Cleanup
+
+        """
+        miroRegions = mirolib.launch_miro()
+        s = miroRegions[0] #Sidebar Region
+        m = miroRegions[1] #Mainview Region
+        t = miroRegions[2] #top half screen
+        tl = miroRegions[3] #top left quarter
+        mtb = miroRegions[4] #main title bar
+        
+        #1. add feed
+        url = "http://pculture.org/feeds_test/feed1.rss"
+        feed = "Yahoo Media TEST"
+        term = "third test video"
+        title = "Video 3"
+        
+        #1. add feed
+        mirolib.add_feed(self,t,s,mtb,url,feed)
+        #2. search
+        mirolib.tab_search(self,m,s,term)
+        
+        #3. verify item metadata
+        self.assertTrue(m.exists(title))
+        self.assertTrue(m.exists("This is the third test video"))
+        self.assertTrue(m.exists("http://participatoryculture.org/feeds_test/mike_tv_drawing_cropped.jpg"))
+        self.assertTrue(m.exists("842 B"))
+
+        #verify the links
+        LINKS = {"absolute link": "http://www.google.com", "relative link": "appcast.xml","another relative": "index.php" }
+        for link, linkurl in LINKS.iteritems():
+            if m.exists(link):
+                click(m.getLastMatch())
+                App.open("Firefox")
+                mirolib.shortcut("l")
+                try:
+                    self.assertEqual(Env.getClipboard(),link)
+                except:
+                    self.verificationErrors.append("relative link not opened in browser")
+        #cleanup
+        mirolib.delete_feed(self,m,s,feed)
+        
+ 
+    def tearDown(self):
+        mirolib.handle_crash_dialog(self)
+        self.assertEqual([], self.verificationErrors)
+        
+        
+# Post the output directly to Litmus
+if config.testlitmus == True:
+    suite_list = unittest.getTestCaseNames(Miro_Suite,'test')
+    suite = unittest.TestSuite()
+    for x in suite_list:
+        suite.addTest(Miro_Suite(x))
+
+    buf = StringIO.StringIO()
+    runner = unittest.TextTestRunner(stream=buf)
+    litmusresult.write_header(config.get_os_name())
+    for x in suite:
+        runner.run(x)
+        # check out the output
+        byte_output = buf.getvalue()
+        id_string = str(x)
+        stat = byte_output[0]
+        try:
+            print "writing log"
+            litmusresult.write_log(id_string,stat,byte_output)
+        finally:
+            buf.truncate(0)
+    litmusresult.write_footer()
+#or just run it locally
+else:
+    unittest.main()
