518a5e8cf393357f20093c912f61acdfd55e4433
janet <jed@pculture.org>
2011-06-05 11:12:18 +0200

    no sg

--- run_suite.py
+++ run_suite.py
@@ -32,7 +32,7 @@ if os.getenv("SIKULI_HOME") == None:
 jar_path = os.path.join(os.getenv("SIKULI_HOME"),"sikuli-script.jar")
 
 
-for subgroup in glob.glob(os.path.join(os.getcwd(), 'sg*.sikuli')):
+for subgroup in glob.glob(os.path.join(os.getcwd(), '*.sikuli')):
     sik_run_cmd = ['java', '-jar', jar_path]
     print os.path.basename(subgroup)
     sik_run_cmd.append(os.path.basename(subgroup))
