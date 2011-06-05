c6274b93d367d5303410880a6aaa38094df7aaa4
janet <jed@pculture.org>
2011-06-05 09:54:18 +0200

    set osx env var

--- run_suite.py
+++ run_suite.py
@@ -18,6 +18,7 @@ qt = options.quicktest
 ###Run all the subgroups:
 
 ##if sys.platform.startswith("darwin"):
+os.setenv("MACOSX_DEPLOYMENT_TARGET","10.5")
 if os.getenv("PCF_TEST_HOME") == None:
     raw_input("Must set PCF_TEST_HOME env to current dir, press key to exit")
 ##else:
@@ -31,7 +32,7 @@ if os.getenv("SIKULI_HOME") == None:
 jar_path = os.path.join(os.getenv("SIKULI_HOME"),"sikuli-script.jar")
 
 
-for subgroup in glob.glob(os.path.join(os.getcwd(), '*.sikuli')):
+for subgroup in glob.glob(os.path.join(os.getcwd(), 'sg*.sikuli')):
     sik_run_cmd = ['java', '-jar', jar_path]
     print os.path.basename(subgroup)
     sik_run_cmd.append(os.path.basename(subgroup))
