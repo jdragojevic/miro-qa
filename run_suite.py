d2083e1b69f7e12ee769eadf2c793c7cb90cd3a4
janet <jed@pculture.org>
2011-06-09 15:20:07 +0200

    make quicktest run option

--- run_suite.py
+++ run_suite.py
@@ -10,12 +10,10 @@ parser = OptionParser()
 parser.add_option("-q", "--quicktest", action="store_true", dest="quicktest", default=False,
                   help='Runs the quicktest suite')
 
-
-
 (options, args) = parser.parse_args()
 qt = options.quicktest
 
-###Run all the subgroups:
+
 
 ##if sys.platform.startswith("darwin"):
 os.putenv("MACOSX_DEPLOYMENT_TARGET","10.5")
@@ -29,19 +27,44 @@ if os.getenv("SIKULI_HOME") == None:
     raw_input("Must set SIKULI_HOME environment var to dir containing sikuli-script.jar \
               Press any key to exit")
 
+
+QUICK_TESTS = [['sg11_torrents.sikuli', 'test_419', 'test_719'],
+               ['sg2_search.sikuli', 'test_82'],
+               ['sg19_system.sikuli', 'test_55'],
+               ['sg_21_sites.sikuli', 'test_182', 'test_143'],
+               ['sg24_shortcuts.sikuli', 'test_92'],
+               ['sg31_playback.sikuli', 'test_160'],
+               ['sg42_feedsearch.sikuli', 'test_720'],
+               ['sg58_items.sikuli', 'test_361'],
+               ['sg6_feeds.sikuli', 'test_338', 'test_117']              
+    ]
+
+
 jar_path = os.path.join(os.getenv("SIKULI_HOME"),"sikuli-script.jar")
-
-
-for subgroup in glob.glob(os.path.join(os.getcwd(), '*.sikuli')):
-    sik_run_cmd = ['java', '-jar', jar_path]
-    print os.path.basename(subgroup)
-    sik_run_cmd.append(os.path.basename(subgroup))
-    p = subprocess.Popen(sik_run_cmd).communicate()
+if qt == True:
+    # runs the quicktest suite
+    #To run just one test:
+    #java -jar "$SIKULI_HOME/sikuli-script.jar" sg_xx_xxx.sikuli test_x test_y ...")
+    for tests in QUICK_TESTS:
+        sik_run_cmd = ['java', '-jar', jar_path]
+        for x in tests:
+            sik_run_cmd.append(x)
+            print "running... ",tests
+        print sik_run_cmd
+        p = subprocess.Popen(sik_run_cmd).communicate()
+else:
+    #Run all the subgroups:
+    #To run all the tests in 1 subgroup:
+    #java -jar $SIKULI_HOME/sikuli-script.jar sgxx_xxx.sikuli"
     
+    for subgroup in glob.glob(os.path.join(os.getcwd(), '*.sikuli')):
+        jar_path = os.path.join(os.getenv("SIKULI_HOME"),"sikuli-script.jar")
+        sik_run_cmd = ['java', '-jar', jar_path]
+        print os.path.basename(subgroup)
+        sik_run_cmd.append(os.path.basename(subgroup))
+        p = subprocess.Popen(sik_run_cmd).communicate()
+        
 
-
-#To run just one test:
-##os.system("java -jar $SIKULI_HOME/sikuli-script.jar test.sikuli test_x test_y ...")
 
     
 
