78cdb81c9f8fa144aa1f8be58b305fe01d8aff04
janet <jed@pculture.org>
2011-07-01 23:07:10 +0200

    way to get and set miro db values

---
+++ db_mod.py
@@ -0,0 +1,64 @@
+import os
+import sys
+import sqlite3
+import pickle
+
+
+
+class MiroDatabase():
+    """Connect to the miro database and set or retrieve values.
+
+    """
+
+    def get_db_location(self):
+        if sys.platform.startswith("darwin"):
+            mirodb = os.path.join(os.getenv("HOME"),"Library","Application Support","Miro","sqlitedb")
+        elif sys.platform.startswith("linux"):
+            mirodb = os.path.join(os.getenv("HOME"),".miro","sqlitedb")
+        elif sys.platform.startswith("win"):
+            #for winXP
+            if "winXP":
+                mirodb = os.path.join(os.getenv("HOME"),"Application Support","Participatory Culture Foundation","Miro","Support","sqlitedb")
+            else:
+                mirodb = os.path.join(os.getenv("HOME"),"AppData","Roaming","Participatory Culture Foundation","Miro","Support","sqlitedb")
+        else:
+            print "no idea what is db"
+            
+        return mirodb
+
+    def get_value(self,table,field):
+        database = self.get_db_location()
+        conn = sqlite3.connect(database)
+        stmt = "select "+field+" from " +table
+        print stmt
+        c = conn.cursor()
+        c.execute(stmt)
+        myval = c.fetchone()[0]
+        conn.close()
+        ofile = os.path.join(os.getenv("PCF_TEST_HOME"),"Miro","dbval.pkl")
+        output = open(ofile, 'wb')
+        pickle.dump(str(myval), output)
+        output.close()
+        
+
+
+    def set_value(self,table,field,val):
+        database = self.get_db_location()
+        conn = sqlite3.connect(database)
+        v = (val,)
+#        c = conn.cursor()
+        conn.execute('update '+table+' set '+field+'=?',v)
+        conn.commit()
+        
+        conn.close()
+
+
+if __name__ == '__main__':
+    print "running"
+##    md =  MiroDatabase()
+##    md.set_value("global_state","tabs_width",300)
+##    md.get_value("global_state","tabs_width")
+
+
+
+        
