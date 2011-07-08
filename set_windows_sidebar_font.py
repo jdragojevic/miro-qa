1e0c370dca5754efd1de05f612d200f6352e25ec
janet <jed@pculture.org>
2011-07-09 00:19:38 +0200

    use programfiles env var on windows

--- set_windows_sidebar_font.py
+++ set_windows_sidebar_font.py
@@ -7,11 +7,11 @@ import shutil
 def set_fonts():
     searchExp = "tahoma 11"
     replaceExp = "Segoe UI 12"
-    infile = os.path.join(os.getenv("PROGRAMFILES","Participatory Culture Foundation\Miro\etc\gtk-2.0\gtkrc"))
-    tmpfile = os.path.join(os.getenv("PROGRAMFILES","Participatory Culture Foundation\Miro\etc\gtk-2.0\gtkrc.mod"))
+    infile = os.path.join(os.getenv("PROGRAMFILES"),"Participatory Culture Foundation\Miro\etc\gtk-2.0\gtkrc")
+    tmpfile = os.path.join(os.getenv("PROGRAMFILES"),"Participatory Culture Foundation\Miro\etc\gtk-2.0\gtkrc.mod")
     fin = open(infile)
     fout = open(tmpfile,"wt")
-    fback = os.path.join(os.getenv("PROGRAMFILES","Participatory Culture Foundation\Miro\etc\gtk-2.0\gtkrc.bak"))
+    fback = os.path.join(os.getenv("PROGRAMFILES"),"Participatory Culture Foundation\Miro\etc\gtk-2.0\gtkrc.bak")
     for line in fin:
         fout.write( line.replace(searchExp, replaceExp) )
     fin.close()
