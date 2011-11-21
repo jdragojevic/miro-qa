3b7566b142059e896906c626a6573643f3a8414b
Janet <janet@macbook-pro.lan>
2011-11-21 14:23:50 +0100

    output results in xunit format for jenkins

---
+++ TestRunner.py
@@ -0,0 +1,26 @@
+#Test Runner making use of the XmlTestRunner by by Sebastian Rittau <srittau@jroger.in-berlin.de>
+
+import sys
+import unittest
+import xmlrunner
+
+
+
+class TestRunner(unittest.TestCase):
+    def __init__(self, suiteclass):
+        print sys.argv
+        if len(sys.argv) > 1:
+            args = sys.argv[1:]
+            print args
+            self.suite = unittest.TestSuite()
+            for x in args:
+                self.suite.addTest(suiteclass(x))
+        else:
+            self.suite = unittest.TestLoader().loadTestsFromTestCase(suiteclass)
+        self.suitename = sys.argv[0].split('.sikuli')[0]
+                    
+    def run_tests(self):
+        r = xmlrunner.XMLTestRunner()
+        r.run(self.suite, self.suitename)
+
+
