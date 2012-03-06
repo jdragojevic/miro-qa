45c301e55522b174131695796678cd9304bfaed1
janetPCF <jed@pculture.org>
2012-03-06 16:40:38 +0100

    set sidebar font better

--- set_windows_sidebar_font.py
+++ set_windows_sidebar_font.py
@@ -1,22 +1,10 @@
-# set the default sidebar font for windows to Segoe UI 13.
+# set the default sidebar font for windows to Segoe UI 12.
 import sys
 import os
-import fileinput
+
 import shutil
 
-def set_fonts():
-    searchExp = "tahoma 11"
-    replaceExp = "Segoe UI 12"
-    infile = os.path.join(os.getenv("PROGRAMFILES"),"Participatory Culture Foundation\Miro\etc\gtk-2.0\gtkrc")
-    tmpfile = os.path.join(os.getenv("PROGRAMFILES"),"Participatory Culture Foundation\Miro\etc\gtk-2.0\gtkrc.mod")
-    fin = open(infile)
-    fout = open(tmpfile,"wt")
-    fback = os.path.join(os.getenv("PROGRAMFILES"),"Participatory Culture Foundation\Miro\etc\gtk-2.0\gtkrc.bak")
-    for line in fin:
-        fout.write( line.replace(searchExp, replaceExp) )
-    fin.close()
-    fout.close()
-    shutil.move(infile,fback)
-    shutil.move(tmpfile,infile)
 
-set_fonts()
+infile = os.path.join(os.getenv("PROGRAMFILES"),"Participatory Culture Foundation","Miro", "etc", "gtk-2.0", "gtkrc")
+fontfile = os.path.join(os.getcwd(),"gtkrc_with_font")
+shutil.copy(fontfile, infile)
